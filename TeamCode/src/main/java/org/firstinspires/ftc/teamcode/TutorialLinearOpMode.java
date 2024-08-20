0d9755c60330fc06b517238f5743cbad414eb2aa
Frank Nguyen <rf2499@hotmail.com>
2024-08-20 02:00:20 -0700

    Added ERCLogger class to handle logging the robot code messages to both Logcat and driver hub. This helps to really clean up the robot code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TutorialLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TutorialLinearOpMode.java
@@ -1,9 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import static android.os.SystemClock.sleep;
-
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 import android.util.Log;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -13,60 +12,31 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 //public class Tutorial extends OpMode {
 public class TutorialLinearOpMode extends LinearOpMode {
 
-    DcMotor motor;
-
+    ERCLogger _logger = new ERCLogger(this);
+    DcMotor _motor;
     int _timeSeconds;
-    String _logTag = "Eastlake";
 
     // Code inside this method will run exactly once after you press the INIT button.
     // This is where you should put all code for the OpMode.
     // https://gm0.org/en/latest/docs/software/getting-started/linear-opmode-vs-opmode.html
     @Override
     public void runOpMode() throws InterruptedException {
+
         String msg = "Init button was pressed. Waiting for start button to be pressed";
-        writeMsgToLog(_logTag, msg); writeMsgToDriverHub(_logTag, msg);
+        _logger.writeMsgToAllLogOutputs(msg);
         waitForStart();
         msg = "start button was pressed";
-        writeMsgToLog(_logTag, msg); writeMsgToLog(_logTag, msg);
+        _logger.writeMsgToAllLogOutputs(msg);
 
         while (!isStopRequested())
         {
             sleep(1000);
             _timeSeconds++;
             msg = "timer: " + _timeSeconds + " seconds";
-            writeMsgToLog(_logTag, msg); writeMsgToLog(_logTag, msg);
+            _logger.writeMsgToAllLogOutputs(msg);
         }
 
         msg = "stop request was detected";
-        writeMsgToLog(_logTag, msg); writeMsgToLog(_logTag, msg);
-    }
-
-//    @Override
-//    public void init() {
-//        _timeSeconds = 0;
-//        //writeMsgToDriverHub("Initialization: ", "is a success");
-//        writeMsgToLog("Initialization: ", "is a success");
-//    }
-//
-//    @Override
-//    public void loop() {
-//        sleep(1000);
-//        _timeSeconds++;
-//        //writeMsgToDriverHub("TimerCount: ", _timeSeconds + " seconds");
-//        writeMsgToLog("TimerCount: ", _timeSeconds + " seconds");
-//    }
-
-    public void writeMsgToLog(String tag, String msg)
-    {
-        // Send text to the logcat window
-        // Note: use [tag: "Eastlake"] to filter on only this code's logs
-        Log.d(tag, msg);
-    }
-
-    public void writeMsgToDriverHub(String caption, Object value)
-    {
-        // Send text to the driver hub
-        telemetry.addData(caption, value);
-        telemetry.update();
+        _logger.writeMsgToAllLogOutputs(msg);
     }
 }
